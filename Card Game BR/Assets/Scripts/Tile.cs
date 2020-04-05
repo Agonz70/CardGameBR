05d23b465172f90d1aece35bee0d0788d84c7f4e
AndrewXR <aig70@miami.edu>
2020-04-04 20:58:46 -0400

    Added TileClass with tile data, tile prefab, and BoardClass

---
+++ Card Game BR/Assets/Scripts/Tile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool cursorhighlighting { get; private set; }
+    public int type { get; private set; }
+    public bool occupied { get; private set; }
+
+    public void InitiliazeTileData()
+    {
+        cursorhighlighting = false;
+        type = 0;
+        occupied = false;
+    }
+
+    public void Print()
+    {
+        Debug.Log(cursorhighlighting);
+        Debug.Log(type);
+        Debug.Log(occupied);
+    }
+
+    public static GameObject InstantiateTile(GameObject prefab, Vector3 worldPosition, Transform board)
+    {
+        GameObject tile = Instantiate(prefab, worldPosition, Quaternion.identity, board);
+        tile.name = worldPosition.x + ", " + worldPosition.y;
+        return tile;
+    }
+
+
+}
