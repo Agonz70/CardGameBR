920842b8d6ada7bc53ee8291343d3323e60775f1
Renel Desir <45770934+rendevelop@users.noreply.github.com>
2020-04-04 23:00:15 -0400

    Added Unit class

---
+++ Card Game BR/Assets/Scripts/StateMachine/Card.cs
@@ -0,0 +1,17 @@
+/***
+  Dummy Card Class for the purpose of the Unit class
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+  private int attack_stat {get; };
+  private int defense_stat {get; };
+  private int mana_required {get; };
+  private String type {get; };
+  private String unit_type {get; };
+
+}
